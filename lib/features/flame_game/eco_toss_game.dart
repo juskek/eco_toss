c564cf39ee417ea0e5e29854493392281b011121
Justin <57725347+juskek@users.noreply.github.com>
2024-01-13 14:54:22 +0000

    feat: create EcoTossGame

---
+++ lib/features/flame_game/eco_toss_game.dart
@@ -0,0 +1,16 @@
+import 'package:endless_runner/features/flame_game/eco_toss_camera_component.dart';
+import 'package:endless_runner/features/flame_game/eco_toss_world.dart';
+import 'package:flame/game.dart';
+
+class EcoTossGame extends FlameGame {
+  EcoTossGame()
+      : super(
+          world: EcoTossWorld(),
+          camera: EcoTossCameraComponent(),
+        );
+
+  @override
+  Future<void> onLoad() async {
+    // camera.backdrop.add(Background(speed: world.speed));
+  }
+}
--- lib/pages/flame_playground/flame_playground_page.dart
+++ lib/pages/flame_playground/flame_playground_page.dart
@@ -1,13 +1,9 @@
+import 'package:endless_runner/features/flame_game/eco_toss_game.dart';
 import 'package:endless_runner/pages/flame_game/endless_runner.dart';
-import 'package:endless_runner/pages/flame_game/game_win_dialog.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:nes_ui/nes_ui.dart';
-import 'package:provider/provider.dart';
 
 import '../../features/audio/audio_controller.dart';
-import '../../features/player_progress/player_progress.dart';
 import '../level_selection/levels.dart';
 
 /// This widget defines the properties of the game screen.
@@ -25,34 +21,11 @@ class FlamePlaygroundPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final audioController = context.read<AudioController>();
+    // final audioController = context.read<AudioController>();
     return Scaffold(
-      body: GameWidget<EndlessRunner>(
+      body: GameWidget<EcoTossGame>(
         key: const Key('play session'),
-        game: EndlessRunner(
-          level: level,
-          playerProgress: context.read<PlayerProgress>(),
-          audioController: audioController,
-        ),
-        overlayBuilderMap: {
-          backButtonKey: (BuildContext context, EndlessRunner game) {
-            return Positioned(
-              top: 20,
-              right: 10,
-              child: NesButton(
-                type: NesButtonType.normal,
-                onPressed: GoRouter.of(context).pop,
-                child: NesIcon(iconData: NesIcons.leftArrowIndicator),
-              ),
-            );
-          },
-          winDialogKey: (BuildContext context, EndlessRunner game) {
-            return GameWinDialog(
-              level: level,
-              levelCompletedIn: game.world.levelCompletedIn,
-            );
-          },
-        },
+        game: EcoTossGame(),
       ),
     );
   }
