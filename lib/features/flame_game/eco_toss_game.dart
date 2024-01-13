c7bb4249bf19d5d87b4928793f961e1202aa5e2c
Justin <57725347+juskek@users.noreply.github.com>
2024-01-13 15:19:47 +0000

    feat: revert background colour to black and add white rectangle

--- lib/features/flame_game/eco_toss_game.dart
+++ lib/features/flame_game/eco_toss_game.dart
@@ -1,7 +1,6 @@
 import 'package:endless_runner/features/flame_game/eco_toss_camera_component.dart';
 import 'package:endless_runner/features/flame_game/eco_toss_world.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
 
 class EcoTossGame extends FlameGame {
   EcoTossGame()
@@ -11,9 +10,5 @@ class EcoTossGame extends FlameGame {
         );
 
   @override
-  Future<void> onLoad() async {
-    // camera.backdrop.add(Background(speed: world.speed));
-  }
-  @override
-  Color backgroundColor() => Colors.white;
+  Future<void> onLoad() async {}
 }
--- lib/features/flame_game/eco_toss_world.dart
+++ lib/features/flame_game/eco_toss_world.dart
@@ -1,6 +1,15 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 
 class EcoTossWorld extends World {
   @override
-  Future<void> onLoad() async {}
+  Future<void> onLoad() async {
+    add(RectangleComponent(
+      position: Vector2(200, 50),
+      size: Vector2.all(30),
+      angle: pi / 4,
+      anchor: Anchor.center,
+    ));
+  }
 }
