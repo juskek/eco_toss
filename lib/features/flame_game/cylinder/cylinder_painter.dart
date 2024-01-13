b03f708466cd4c46e1001ef3ed892b7148dfbbef
Justin <57725347+juskek@users.noreply.github.com>
2024-01-13 17:11:04 +0000

    feat: PoC for 3d cylinder

--- lib/features/flame_game/cylinder/cylinder_painter.dart
+++ lib/features/flame_game/cylinder/cylinder_painter.dart
@@ -5,11 +5,32 @@ import 'package:flutter/material.dart';
 class CylinderCustomPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final paintStyle = Paint()..color = Colors.red;
+    final paintStyle = Paint()..color = Colors.white;
+    final redPaintStyle = Paint()..color = Colors.red;
+    const circleOffset = 30.0;
+    const rotationX = pi / 4;
+    const rotationY = pi / 4;
+    const rotationZ = pi / 4;
     canvas
-      ..transform(Matrix4.rotationX(pi / 4).storage)
-      ..drawRect(Rect.fromCenter(center: Offset.zero, width: 30, height: 30),
+      ..transform(Matrix4.rotationX(rotationX).storage)
+      ..transform(Matrix4.rotationY(rotationY).storage)
+      ..transform(Matrix4.rotationZ(rotationZ).storage)
+      ..drawCircle(const Offset(0, circleOffset), 15, redPaintStyle);
+    canvas.restore();
+    canvas
+      ..transform(Matrix4.rotationX(rotationX).storage)
+      ..transform(Matrix4.rotationY(rotationY).storage)
+      ..transform(Matrix4.rotationZ(rotationZ).storage)
+      ..drawRect(Rect.fromCenter(center: Offset.zero, width: 30, height: 60),
           paintStyle);
+    canvas.restore();
+    canvas
+      ..transform(Matrix4.rotationX(rotationX).storage)
+      ..transform(Matrix4.rotationY(rotationY).storage)
+      ..transform(Matrix4.rotationZ(rotationZ).storage)
+      ..drawCircle(const Offset(0, -circleOffset), 15, redPaintStyle);
+
+    canvas.restore();
   }
 
   @override
