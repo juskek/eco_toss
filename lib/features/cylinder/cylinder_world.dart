131f3127a33cb400fefcf2161e1e8cce7ae15a50
Justin <57725347+juskek@users.noreply.github.com>
2024-01-14 09:06:27 +0000

    feat: add rotationsAround X and Z

--- lib/features/cylinder/cylinder_world.dart
+++ lib/features/cylinder/cylinder_world.dart
@@ -6,25 +6,78 @@ import 'package:flame/components.dart';
 
 class CylinderWorld extends World {
   FutureOr<void> addRotationsAroundY() async {
-    add(PositionComponent(position: Vector2(-100, -300), children: [
-      CylinderComponent(rotationX: 0, rotationY: -pi, rotationZ: 0)
-    ]));
-    add(PositionComponent(position: Vector2(-50, -300), children: [
-      CylinderComponent(rotationX: 0, rotationY: -pi / 2, rotationZ: 0)
-    ]));
-    add(PositionComponent(position: Vector2(-0, -300), children: [
-      CylinderComponent(rotationX: 0, rotationY: 0, rotationZ: 0)
-    ]));
-    add(PositionComponent(position: Vector2(50, -300), children: [
-      CylinderComponent(rotationX: 0, rotationY: pi / 2, rotationZ: 0)
-    ]));
-    add(PositionComponent(position: Vector2(100, -300), children: [
-      CylinderComponent(rotationX: 0, rotationY: pi, rotationZ: 0)
-    ]));
+    const int numberOfComponents = 5;
+    const double startPositionX = -100;
+    const double positionIncrementX = 50;
+    const double positionY = -200;
+
+    for (int i = 0; i < numberOfComponents; i++) {
+      double positionX = startPositionX + (i * positionIncrementX);
+      double rotation = -pi + (i * (pi / 2));
+
+      add(PositionComponent(
+        position: Vector2(positionX, positionY),
+        children: [
+          CylinderComponent(
+            rotationX: 0,
+            rotationY: rotation,
+            rotationZ: 0,
+          ),
+        ],
+      ));
+    }
+  }
+
+  FutureOr<void> addRotationsAroundX() async {
+    const int numberOfComponents = 5;
+    const double startPositionX = -100;
+    const double positionIncrementX = 50;
+    const double positionY = -300;
+
+    for (int i = 0; i < numberOfComponents; i++) {
+      double positionX = startPositionX + (i * positionIncrementX);
+      double rotation = -pi + (i * (pi / 2));
+
+      add(PositionComponent(
+        position: Vector2(positionX, positionY),
+        children: [
+          CylinderComponent(
+            rotationX: rotation,
+            rotationY: 0,
+            rotationZ: 0,
+          ),
+        ],
+      ));
+    }
+  }
+
+  FutureOr<void> addRotationsAroundZ() async {
+    const int numberOfComponents = 5;
+    const double startPositionX = -100;
+    const double positionIncrementX = 50;
+    const double positionY = -100;
+
+    for (int i = 0; i < numberOfComponents; i++) {
+      double positionX = startPositionX + (i * positionIncrementX);
+      double rotation = -pi + (i * (pi / 2));
+
+      add(PositionComponent(
+        position: Vector2(positionX, positionY),
+        children: [
+          CylinderComponent(
+            rotationX: 0,
+            rotationY: 0,
+            rotationZ: rotation,
+          ),
+        ],
+      ));
+    }
   }
 
   @override
   Future<void> onLoad() async {
+    addRotationsAroundX();
     addRotationsAroundY();
+    addRotationsAroundZ();
   }
 }
