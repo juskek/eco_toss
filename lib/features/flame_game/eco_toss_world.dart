ce774bc38234d32dd9e172557a6ed8699283122b
Justin <57725347+juskek@users.noreply.github.com>
2024-01-19 16:20:34 +0000

    feat: add z dimensions

--- lib/features/flame_game/eco_toss_world.dart
+++ lib/features/flame_game/eco_toss_world.dart
@@ -1,7 +1,3 @@
-import 'package:endless_runner/features/flame_game/backboard/backboard.dart';
-import 'package:endless_runner/features/flame_game/ball/ball.dart';
-import 'package:endless_runner/features/flame_game/bin/bin_component.dart';
-import 'package:endless_runner/features/flame_game/physics/physics.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
@@ -14,48 +10,62 @@ class EcoTossWorld extends World with HasCollisionDetection, HasGameRef {
 
   @override
   Future<void> onLoad() async {
-    showXandYDimensions();
-    await add(RectangleComponent(
-      position: Vector2(0, yFloorPixels),
-      size: Vector2(findGame()!.canvasSize.x, 10),
-      anchor: Anchor.center,
-    ));
-    await add(BackboardComponent(
-      size: Vector2(findGame()!.canvasSize.x * 0.3, 100),
-    ));
-    final binComponent =
-        BinComponent(size: Vector2(findGame()!.canvasSize.x * 0.3, 200));
-    await add(binComponent);
-    await add(BallComponent(
-      radiusStart: 50,
-      addScore: addScore,
-    ));
+    showXYZDimensions();
+    // await add(RectangleComponent(
+    //   position: Vector2(0, yFloorPixels),
+    //   size: Vector2(findGame()!.canvasSize.x, 10),
+    //   anchor: Anchor.center,
+    // ));
+    // await add(BackboardComponent(
+    //   size: Vector2(findGame()!.canvasSize.x * 0.3, 100),
+    // ));
+    // final binComponent =
+    //     BinComponent(size: Vector2(findGame()!.canvasSize.x * 0.3, 200));
+    // await add(binComponent);
+    // await add(BallComponent(
+    //   radiusStart: 50,
+    //   addScore: addScore,
+    // ));
 
-    final ballNotifier = gameRef.componentsNotifier<BallComponent>();
-    ballNotifier.addListener(() {
-      final ball = ballNotifier.single;
-      if (ball == null) {
-        binComponent.priority = 1;
-        add(BallComponent(
-          radiusStart: 50,
-          addScore: addScore,
-        ));
-      }
-      if (ball != null && ball.zPositionMetres >= zBinStartMetres) {
-        binComponent.priority = 2;
-        ball.priority = 1;
-      }
-    });
+    // final ballNotifier = gameRef.componentsNotifier<BallComponent>();
+    // ballNotifier.addListener(() {
+    //   final ball = ballNotifier.single;
+    //   if (ball == null) {
+    //     binComponent.priority = 1;
+    //     add(BallComponent(
+    //       radiusStart: 50,
+    //       addScore: addScore,
+    //     ));
+    //   }
+    //   if (ball != null && ball.zPositionMetres >= zBinStartMetres) {
+    //     binComponent.priority = 2;
+    //     ball.priority = 1;
+    //   }
+    // });
   }
 
-  void showXandYDimensions() {
+  void showXYZDimensions() {
     final canvasWidthX = findGame()!.canvasSize.x;
     final leftX = -canvasWidthX / 2;
     final rightX = canvasWidthX / 2;
     final canvasHeightY = findGame()!.canvasSize.y;
     final bottomY = canvasHeightY / 2;
     final topY = -canvasHeightY / 2;
-
+    for (var x = leftX; x < rightX; x += 100) {
+      add(
+        TextComponent(
+          text: 'xPixels: ${x.toInt()}',
+          position: Vector2(x, 0),
+          anchor: Anchor.center,
+          textRenderer: TextPaint(
+            style: const TextStyle(
+              fontSize: 10,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+    }
     for (var y = topY; y < bottomY; y += 100) {
       add(
         TextComponent(
@@ -70,13 +80,12 @@ class EcoTossWorld extends World with HasCollisionDetection, HasGameRef {
         ),
       );
     }
-
-    for (var x = leftX; x < rightX; x += 100) {
+    for (var z = topY; z < bottomY; z += 100) {
       add(
         TextComponent(
-          text: 'xPixels: ${x.toInt()}',
-          position: Vector2(x, 0),
-          anchor: Anchor.center,
+          text: 'zPixels: ${z.toInt()}',
+          position: Vector2(rightX, z),
+          anchor: Anchor.centerRight,
           textRenderer: TextPaint(
             style: const TextStyle(
               fontSize: 10,
