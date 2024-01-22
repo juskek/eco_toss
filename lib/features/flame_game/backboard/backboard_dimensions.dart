a7e0e1d8f4514e9548978b3f83f4fba58d96b883
Justin <57725347+juskek@users.noreply.github.com>
2024-01-22 11:06:48 +0000

    feat: increase size of backboard

--- lib/features/flame_game/backboard/backboard_dimensions.dart
+++ lib/features/flame_game/backboard/backboard_dimensions.dart
@@ -1,4 +1,4 @@
 abstract class BackboardDimensions {
-  static const heightMetres = 0.3;
-  static const widthMetres = 0.3;
+  static const heightMetres = 1.0;
+  static const widthMetres = 1.0;
 }
--- lib/features/flame_game/bin/bin_dimensions.dart
+++ lib/features/flame_game/bin/bin_dimensions.dart
@@ -1,5 +1,5 @@
 abstract class BinDimensions {
-  static const heightMetres = 0.5;
-  static const widthMetres = 0.3;
+  static const heightMetres = 1.2;
+  static const widthMetres = 1.0;
   static const depthMetres = 0.2;
 }
