becd72caf7ef3f06121d111ee97a36eb4cca250c
Justin Kek <justinkekjq@gmail.com>
2024-02-05 08:46:52 +0800

    build: add injectable

---
+++ lib/common_imports.dart
@@ -0,0 +1,4 @@
+export 'package:eco_toss/ioc/dependency_injection.dart';
+export 'package:flutter/foundation.dart';
+export 'package:flutter/material.dart';
+export 'package:injectable/injectable.dart';
---
+++ lib/ioc/dependency_injection.config.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+import '../data/app_info/app_info_repository.dart' as _i4;
+import '../data/app_info/i_app_info_repository.dart' as _i3;
+
+const String _prod = 'prod';
+
+// initializes the registration of main-scope dependencies inside of GetIt
+_i1.GetIt $initGetIt(
+  _i1.GetIt getIt, {
+  String? environment,
+  _i2.EnvironmentFilter? environmentFilter,
+}) {
+  final gh = _i2.GetItHelper(
+    getIt,
+    environment,
+    environmentFilter,
+  );
+  gh.singleton<_i3.IAppInfoRepository>(
+    _i4.AppInfoRepository(),
+    registerFor: {_prod},
+  );
+  return getIt;
+}
