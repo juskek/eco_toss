2283610709fa89ae815eda4e31885c40258cf258
Justin Kek <justinkekjq@gmail.com>
2024-02-09 15:24:59 +0800

    feat: show spinning ball on HomePage

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:flame/components.dart';
+import 'package:flame/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -26,9 +28,19 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                'assets/images/banner.png',
-                filterQuality: FilterQuality.none,
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: SpriteAnimationWidget.asset(
+                  path: 'throwables/paper_ball.png',
+                  playing: true,
+                  data: SpriteAnimationData.sequenced(
+                    amount: 20,
+                    textureSize: Vector2.all(128),
+                    texturePosition: Vector2(512, 0),
+                    stepTime: 0.1,
+                  ),
+                ),
               ),
               _gap,
               ConstrainedBox(
