2ad0c2af64d6119f5f798e259c6749fc25fa5fe6
Justin Kek <justinkekjq@gmail.com>
2024-02-10 22:45:58 +0800

    fix: add SafeArea to LeaderboardPage

--- lib/pages/leaderboard_page/leaderboard_page.dart
+++ lib/pages/leaderboard_page/leaderboard_page.dart
@@ -23,87 +23,91 @@ class LeaderboardPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: palette.backgroundLevelSelection.color,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Select level',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  const SizedBox(width: 16),
-                  NesButton(
-                    type: NesButtonType.normal,
-                    child: NesIcon(iconData: NesIcons.questionMark),
-                    onPressed: () {
-                      NesDialog.show(
-                        context: context,
-                        builder: (_) => const InstructionsDialog(),
-                      );
-                    },
-                  )
-                ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Leaderboard',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    const SizedBox(width: 16),
+                    NesButton(
+                      type: NesButtonType.normal,
+                      child: NesIcon(iconData: NesIcons.questionMark),
+                      onPressed: () {
+                        NesDialog.show(
+                          context: context,
+                          builder: (_) => const InstructionsDialog(),
+                        );
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 50),
-          Expanded(
-            child: SizedBox(
-              width: 450,
-              child: ListView(
-                children: [
-                  for (final level in gameLevels)
-                    ListTile(
-                      enabled: playerProgress.levels.length >= level.number - 1,
-                      onTap: () {
-                        final audioController = context.read<AudioController>();
-                        audioController.playSfx(SfxType.buttonTap);
+            const SizedBox(height: 50),
+            Expanded(
+              child: SizedBox(
+                width: 450,
+                child: ListView(
+                  children: [
+                    for (final level in gameLevels)
+                      ListTile(
+                        enabled:
+                            playerProgress.levels.length >= level.number - 1,
+                        onTap: () {
+                          final audioController =
+                              context.read<AudioController>();
+                          audioController.playSfx(SfxType.buttonTap);
 
-                        GoRouter.of(context)
-                            .go('/play/session/${level.number}');
-                      },
-                      leading: Text(
-                        level.number.toString(),
-                        style: levelTextStyle,
-                      ),
-                      title: Row(
-                        children: [
-                          Text(
-                            'Level #${level.number}',
-                            style: levelTextStyle,
-                          ),
-                          if (playerProgress.levels.length <
-                              level.number - 1) ...[
-                            const SizedBox(width: 10),
-                            const Icon(Icons.lock, size: 20),
-                          ] else if (playerProgress.levels.length >=
-                              level.number) ...[
-                            const SizedBox(width: 50),
+                          GoRouter.of(context)
+                              .go('/play/session/${level.number}');
+                        },
+                        leading: Text(
+                          level.number.toString(),
+                          style: levelTextStyle,
+                        ),
+                        title: Row(
+                          children: [
                             Text(
-                              '${playerProgress.levels[level.number - 1]}s',
+                              'Level #${level.number}',
                               style: levelTextStyle,
                             ),
+                            if (playerProgress.levels.length <
+                                level.number - 1) ...[
+                              const SizedBox(width: 10),
+                              const Icon(Icons.lock, size: 20),
+                            ] else if (playerProgress.levels.length >=
+                                level.number) ...[
+                              const SizedBox(width: 50),
+                              Text(
+                                '${playerProgress.levels[level.number - 1]}s',
+                                style: levelTextStyle,
+                              ),
+                            ],
                           ],
-                        ],
-                      ),
-                    )
-                ],
+                        ),
+                      )
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 30),
-          WobblyButton(
-            onPressed: () {
-              GoRouter.of(context).go('/');
-            },
-            child: const Text('Back'),
-          ),
-          const SizedBox(height: 30),
-        ],
+            const SizedBox(height: 30),
+            WobblyButton(
+              onPressed: () {
+                GoRouter.of(context).go('/');
+              },
+              child: const Text('Back'),
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
