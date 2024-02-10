dd523b22d1fcd16a3a109da9d3fb8f1b4fc2101a
Justin Kek <justinkekjq@gmail.com>
2024-02-10 23:07:59 +0800

    feat: basic UI for LeaderboardPage

--- lib/pages/leaderboard_page/leaderboard_page.dart
+++ lib/pages/leaderboard_page/leaderboard_page.dart
@@ -1,15 +1,10 @@
-import 'package:eco_toss/pages/leaderboard_page/instructions_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:nes_ui/nes_ui.dart';
 import 'package:provider/provider.dart';
 
 import '../../atomic/palette.dart';
 import '../../atomic/wobbly_button.dart';
-import '../../features/audio/audio_controller.dart';
-import '../../features/audio/sounds.dart';
 import '../../features/player_progress/player_progress.dart';
-import 'levels.dart';
 
 class LeaderboardPage extends StatelessWidget {
   const LeaderboardPage({super.key});
@@ -26,30 +21,24 @@ class LeaderboardPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Leaderboard',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    const SizedBox(width: 16),
-                    NesButton(
-                      type: NesButtonType.normal,
-                      child: NesIcon(iconData: NesIcons.questionMark),
-                      onPressed: () {
-                        NesDialog.show(
-                          context: context,
-                          builder: (_) => const InstructionsDialog(),
-                        );
-                      },
-                    )
-                  ],
-                ),
-              ),
+            Text(
+              'Leaderboard',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            Text(
+              'Rank 4',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            const CircleAvatar(
+              radius: 50,
+            ),
+            const Text('Crunchy Granola'),
+            const Text('20 points'),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 16),
+              ],
             ),
             const SizedBox(height: 50),
             Expanded(
@@ -57,40 +46,25 @@ class LeaderboardPage extends StatelessWidget {
                 width: 450,
                 child: ListView(
                   children: [
-                    for (final level in gameLevels)
+                    for (var i = 0; i < 50; i++)
                       ListTile(
-                        enabled:
-                            playerProgress.levels.length >= level.number - 1,
-                        onTap: () {
-                          final audioController =
-                              context.read<AudioController>();
-                          audioController.playSfx(SfxType.buttonTap);
-
-                          GoRouter.of(context)
-                              .go('/play/session/${level.number}');
-                        },
+                        enabled: false,
                         leading: Text(
-                          level.number.toString(),
+                          i.toString(),
                           style: levelTextStyle,
                         ),
                         title: Row(
                           children: [
+                            const CircleAvatar(),
                             Text(
-                              'Level #${level.number}',
+                              'Rando',
                               style: levelTextStyle,
                             ),
-                            if (playerProgress.levels.length <
-                                level.number - 1) ...[
-                              const SizedBox(width: 10),
-                              const Icon(Icons.lock, size: 20),
-                            ] else if (playerProgress.levels.length >=
-                                level.number) ...[
-                              const SizedBox(width: 50),
-                              Text(
-                                '${playerProgress.levels[level.number - 1]}s',
-                                style: levelTextStyle,
-                              ),
-                            ],
+                            const Expanded(child: SizedBox()),
+                            Text(
+                              '1234 points',
+                              style: levelTextStyle,
+                            ),
                           ],
                         ),
                       )
