b3bdd47655aec334eff1e5e3555585e9ed55fd1e
Justin Kek <justinkekjq@gmail.com>
2024-02-14 23:36:36 +0800

    feat: show dialog on Tutorial load and when score hits 3

--- lib/features/flame_game/game_implementations/tutorial_eco_toss_game.dart
+++ lib/features/flame_game/game_implementations/tutorial_eco_toss_game.dart
@@ -1,3 +1,15 @@
 import 'package:eco_toss/features/flame_game/base_eco_toss_game.dart';
+import 'package:eco_toss/pages/tutorial_page/tutorial_page.dart';
 
-class TutorialEcoTossGame extends BaseEcoTossGame {}
+class TutorialEcoTossGame extends BaseEcoTossGame {
+  @override
+  Future<void> onLoad() {
+    overlays.add(TutorialPage.welcomeOverlayKey);
+    world.scoreNotifier.addListener(() {
+      if (world.scoreNotifier.value == 3) {
+        overlays.add(TutorialPage.readyForOnboardingOverlayKey);
+      }
+    });
+    return super.onLoad();
+  }
+}
--- lib/pages/tutorial_page/tutorial_page.dart
+++ lib/pages/tutorial_page/tutorial_page.dart
@@ -1,10 +1,14 @@
 import 'package:eco_toss/features/flame_game/base_eco_toss_game.dart';
 import 'package:eco_toss/features/flame_game/game_implementations/tutorial_eco_toss_game.dart';
+import 'package:eco_toss/pages/flame_game/game_win_dialog.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 class TutorialPage extends StatelessWidget {
   const TutorialPage({super.key});
+
+  static const String welcomeOverlayKey = 'welcome';
+  static const String readyForOnboardingOverlayKey = 'ready_for_onboarding';
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +16,29 @@ class TutorialPage extends StatelessWidget {
       body: GameWidget<BaseEcoTossGame>(
         key: const Key('tutorial'),
         game: TutorialEcoTossGame(),
+        overlayBuilderMap: {
+          welcomeOverlayKey: (BuildContext context, BaseEcoTossGame game) {
+            return const GameWinDialog(
+              level: (
+                number: 1,
+                winScore: 3,
+                canSpawnTall: false,
+              ),
+              levelCompletedIn: 123,
+            );
+          },
+          readyForOnboardingOverlayKey:
+              (BuildContext context, BaseEcoTossGame game) {
+            return const GameWinDialog(
+              level: (
+                number: 1,
+                winScore: 3,
+                canSpawnTall: false,
+              ),
+              levelCompletedIn: 123,
+            );
+          },
+        },
       ),
     );
   }
