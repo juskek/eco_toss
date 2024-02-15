fb6626fa941909efa75d4f37be4ebdb30ce607ef
Justin Kek <justinkekjq@gmail.com>
2024-02-15 10:39:22 +0800

    feat: OnboardingPage UI

--- lib/pages/onboarding/onboarding_page.dart
+++ lib/pages/onboarding/onboarding_page.dart
@@ -1,10 +1,57 @@
+import 'package:eco_toss/atomic/palette.dart';
+import 'package:eco_toss/atomic/wobbly_button.dart';
+import 'package:eco_toss/features/name/change_name_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
+  static const _gap = SizedBox(height: 60);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final palette = context.watch<Palette>();
+
+    return Scaffold(
+      backgroundColor: palette.backgroundSettings.color,
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxWidth: 600,
+                ),
+                child: ListView(
+                  children: const [
+                    _gap,
+                    Text(
+                      'Settings',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Press Start 2P',
+                        fontSize: 30,
+                        height: 1,
+                      ),
+                    ),
+                    _gap,
+                    ChangeNameWidget(),
+                  ],
+                ),
+              ),
+            ),
+            _gap,
+            WobblyButton(
+              onPressed: () {
+                context.go('/');
+              },
+              child: const Text('Back'),
+            ),
+            _gap,
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -34,7 +34,8 @@ abstract class EcoTossRouter {
     assert(_initialRoute != null,
         'Initial route is null, did you forget to call EcoTossRouter.init()?');
     _instance ??= GoRouter(
-      initialLocation: _initialRoute,
+      initialLocation: "/onboarding",
+      // initialLocation: _initialRoute,
       routes: [
         GoRoute(
             path: '/tutorial',
