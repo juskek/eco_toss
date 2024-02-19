99fd75379e37acdf8f5b91aced644467e10cf9d1
Justin Kek <justinkekjq@gmail.com>
2024-02-19 16:07:36 +0000

    feat: calculate hole coordinates

--- lib/features/flame_game/bin/bin_dimensions.dart
+++ lib/features/flame_game/bin/bin_dimensions.dart
@@ -13,8 +13,24 @@ abstract class BinDimensions {
   BinFrontSurfaceComponent frontSurfaceComponent,
   BinBackSurfaceComponent backSurfaceComponent,
 }) createBinComponents({required int midpointXMetres}) {
+  final holeCoordinates =
+      calculateCornerCoordinates(midpointXMetres: midpointXMetres);
   return (
     frontSurfaceComponent: BinFrontSurfaceComponent(),
     backSurfaceComponent: BinBackSurfaceComponent(),
+  );
+}
+
+({
+  double backLeftCorner,
+  double backRightCorner,
+  double frontLeftCorner,
+  double frontRightCorner
+}) calculateCornerCoordinates({required int midpointXMetres}) {
+  return (
+    backLeftCorner: midpointXMetres - BinDimensions.widthMetres / 2,
+    backRightCorner: midpointXMetres + BinDimensions.widthMetres / 2,
+    frontLeftCorner: midpointXMetres - BinDimensions.widthMetres / 2,
+    frontRightCorner: midpointXMetres + BinDimensions.widthMetres / 2,
   );
 }
