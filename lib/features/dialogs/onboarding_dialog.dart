b21c8f3ce2cbdc6451c2938405f6fdd7da6e3605
Justin Kek <justinkekjq@gmail.com>
2024-02-20 10:12:48 +0000

    feat: clean up onboarding dialog

--- lib/features/dialogs/onboarding_dialog.dart
+++ lib/features/dialogs/onboarding_dialog.dart
@@ -1,7 +1,5 @@
+import 'package:eco_toss/atomic/atoms/dialog_atom.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../../atomic/palette.dart';
 
 class OnboardingDialog extends StatelessWidget {
   const OnboardingDialog({
@@ -13,32 +11,26 @@ class OnboardingDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final palette = context.read<Palette>();
-    return Center(
-      child: Container(
-        width: 420,
-        height: 280,
-        color: palette.backgroundPlaySession.color,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Great job!",
-              style: Theme.of(context).textTheme.headlineMedium,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              "Now that you've learned the basics, recyle away!",
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 16),
-            TextButton(
-              onPressed: onPressed,
-              child: const Text("Let's go!"),
-            ),
-          ],
-        ),
+    return DialogAtom(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Great job!",
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Now that you've learned the basics, recyle away!",
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          FilledButton(
+            onPressed: onPressed,
+            child: const Text("Let's go!"),
+          ),
+        ],
       ),
     );
   }
