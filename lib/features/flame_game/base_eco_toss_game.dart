f96dc09dec0cefe55b8479ec88e319fd9c6f2b19
Justin Kek <justinkekjq@gmail.com>
2024-02-20 12:34:11 +0000

    feat: position score text

--- lib/features/flame_game/base_eco_toss_game.dart
+++ lib/features/flame_game/base_eco_toss_game.dart
@@ -1,7 +1,10 @@
 import 'package:eco_toss/features/flame_game/background/background_component.dart';
 import 'package:eco_toss/features/flame_game/background/cloud_component.dart';
 import 'package:eco_toss/features/flame_game/base_eco_toss_world.dart';
+import 'package:eco_toss/features/flame_game/bin/bin_dimensions.dart';
 import 'package:eco_toss/features/flame_game/eco_toss_camera_component.dart';
+import 'package:eco_toss/features/flame_game/physics/physics.dart';
+import 'package:eco_toss/features/flame_game/positioning/positioning.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
@@ -22,15 +25,33 @@ abstract class BaseEcoTossGame extends FlameGame<BaseEcoTossWorld> {
 
   @override
   Future<void> onLoad() async {
-    camera.backdrop.add(BackgroundComponent());
-    camera.backdrop.add(cloudComponent(speed: 20, size: size));
     camera.viewfinder.anchor = Anchor.center;
     camera.viewfinder.position = Vector2(0, 0);
+
+    EcoTossPositioning.setCanvasSize(canvasSize.y, canvasSize.x);
+    camera.backdrop.add(BackgroundComponent());
+    camera.backdrop.add(cloudComponent(speed: 20, size: size));
+
     const scoreText = 'Recycled: 0';
+
+    final textXYZMetres = Vector3(
+        EcoToss3DSpace.xMidMetres,
+        EcoToss3DSpace.yMinMetres + BinDimensions.heightMetres + 0.2,
+        EcoToss3DSpace.zMaxMetres);
+
+    final textXYPixels = EcoTossPositioning.xyzMetresToXyPixels(textXYZMetres);
+
+    // Can't seem to set the origin of the viewport to be the centre of the screen, so this is a workaround
+    final viewportOrigin = Vector2(
+      canvasSize.x / 2,
+      canvasSize.y / 2,
+    );
 
     final scoreComponent = TextComponent(
       text: scoreText,
-      position: Vector2.all(30),
+      position: Vector2(
+          viewportOrigin.x + textXYPixels.x, viewportOrigin.y + textXYPixels.y),
+      anchor: Anchor.center,
       textRenderer: TextPaint(
         style: const TextStyle(
           fontFamily: 'Happy Monkey',
@@ -40,7 +61,7 @@ abstract class BaseEcoTossGame extends FlameGame<BaseEcoTossWorld> {
       ),
     );
 
-    camera.viewport.add(scoreComponent);
+    camera.viewport.add((scoreComponent));
 
     scoreNotifier.addListener(() {
       scoreComponent.text =
