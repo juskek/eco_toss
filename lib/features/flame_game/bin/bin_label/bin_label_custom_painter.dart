a8fc42ca94f6808261d834533944c74aefa31a8a
Justin Kek <justinkekjq@gmail.com>
2024-02-28 22:43:32 +0000

    feat: center bin label image in its canvas

--- lib/features/flame_game/bin/bin_label/bin_label_custom_painter.dart
+++ lib/features/flame_game/bin/bin_label/bin_label_custom_painter.dart
@@ -3,24 +3,31 @@ import 'dart:ui' as ui show Image;
 import 'package:flutter/material.dart';
 
 class BinLabelCustomPainter extends CustomPainter {
+  final bool _showAnchor = true;
+
   final ui.Image image;
   const BinLabelCustomPainter({required this.image});
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
-      ..color = Colors.red.withOpacity(0.5)
+      ..color = Colors.black
       ..style = PaintingStyle.fill;
 
-    final path = Path()
-      ..moveTo(0, 0)
-      ..lineTo(0, size.height)
-      ..lineTo(size.width, size.height)
-      ..lineTo(size.width, 0)
-      ..close();
-    const scale = 0.15;
+    const scale = 0.3;
+    // const scale = 0.15;
     canvas.scale(scale, scale);
-    canvas.drawImage(image, Offset.zero, Paint());
-    canvas.drawPath(path, paint);
+    canvas.drawImage(image, Offset(-size.width / 2, -size.height / 2), Paint());
+
+    if (_showAnchor) {
+      const anchorSize = 15.0;
+      final path = Path()
+        ..moveTo(-anchorSize, -anchorSize)
+        ..lineTo(anchorSize, -anchorSize)
+        ..lineTo(anchorSize, anchorSize)
+        ..lineTo(-anchorSize, anchorSize)
+        ..close();
+      canvas.drawPath(path, paint);
+    }
   }
 
   @override
